b1a309ce6b76cb63b4ee81ed21a9155bdc9db6c4
bitsadzeVazha <vazha.bitsadze11@gmail.com>
2024-04-15 12:20:03 +0400

    Devall Feedback Extension update

--- Devall/CustomerFeedback/view/adminhtml/web/js/grid/columns/anonymous-renderer.js
+++ Devall/CustomerFeedback/view/adminhtml/web/js/grid/columns/anonymous-renderer.js
@@ -9,10 +9,8 @@ define([
         },
 
         getLabel: function (record) {
-            console.log("Original Value: ", this._super());
             var value = this._super();
             var displayValue = parseInt(value) === 1 ? 'Yes' : 'No';
-            console.log("Display Value: ", displayValue);
             return displayValue;
         }
 
